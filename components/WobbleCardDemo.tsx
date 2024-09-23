f442e32ee25e093037d64f4597b77de86b3909af
Nimesh Ranasinghe <111418364+07Nimesh01Ranasinghe@users.noreply.github.com>
2024-09-23 23:53:30 +0530

    Update WobbleCardDemo.tsx

--- components/WobbleCardDemo.tsx
+++ components/WobbleCardDemo.tsx
@@ -12,11 +12,11 @@ export function WobbleCardDemo() {
       >
         <div className="max-w-xs">
           <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Gippity AI powers the entire universe
+          Values
           </h2>
           <p className="mt-4 text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+          We add value to the industry as the business partner through diversified business
+          solutions derived from state of the art technology blended with our well.
           </p>
         </div>
         <Image
@@ -29,21 +29,23 @@ export function WobbleCardDemo() {
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 bg-p-400 min-h-[300px]">
         <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
+            Vision
         </h2>
         <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
+            Un-detachable partner in total solutions.
         </p>
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-p-500 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
         <div className="max-w-sm">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+            Who We Are?
           </h2>
           <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+          Dockyard Total Solutions (Pvt) Ltd (DTS), a subsidiary of Colombo Dockyard 
+          PLC with over 35% government ownership, specializes in ICT systems engineering. 
+          Established under the Companies Act of 2007, DTS provides comprehensive ICT 
+          solutions through its Integrated ICT Solution Center, supported by a team of 
+          25+ skilled engineers, offering 24/7 service with deep industry expertise and a commitment to excellence.
           </p>
         </div>
         <Image
